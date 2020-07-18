6759419e96a4d5b5046a531cd13e88bf84d5c8bf
mphil17 <mattphil48@gmail.com>
2020-07-18 15:21:26 +0000

    added a tick and a cross for right and wrong answers and showed the correct answer if an incorrect answer is given. Reset everything on next question.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,25 +4,29 @@ let answer = [];
 
 function setQuestion() {
     answer.length = 0;
+    $("#right").removeClass("correct").addClass("tick");
+    $("#wrong").removeClass("incorrect").addClass("cross");
+    $("#showAnswer").text("");
     document.getElementById("answer").value = "";
     let multiplication1 = number1[Math.floor(Math.random() * number1.length)];
     let multiplication2 = number2[Math.floor(Math.random() * number2.length)];
     let realAnswer = multiplication1 * multiplication2;
+    $("#question-number").text(`${multiplication1} x ${multiplication2}`)
     answer.push(realAnswer)
-    console.log (realAnswer);
-    console.log (answer);
+  
 }
 
 function answerQuestion() {
     var userAnswer = document.getElementById("answer").value;
     if (answer[0] == userAnswer) {
-        $("#message").text("Well done")
+        $("#right").removeClass("tick").addClass("correct");
     }
     else {
-         $("#message").text("Incorrect")
+         $("#wrong").removeClass("cross").addClass("incorrect");
+         $("#showAnswer").text(answer[0]);
     }
-    console.log(userAnswer)
-    console.log(answer)
+    
+
 }
 
 
