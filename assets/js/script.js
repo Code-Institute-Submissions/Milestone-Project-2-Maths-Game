4cd9b2c32eada1dd3e769b7f1f7f8fef36b69ef8
mphil17 <mattphil48@gmail.com>
2020-07-19 11:19:02 +0000

    fixed bugs on the countdown timer for the three different scenarios- correct answer, incorrect answer and run out of time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 let number1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 let number2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 let answer = [];
+let correctAnswer = false;
+let wrongAnswer = false;
 
 function setQuestion() {
+    correctAnswer = false;
+    wrongAnswer = false;
     answer.length = 0;
     $("#right").removeClass("correct").addClass("tick");
     $("#wrong").removeClass("incorrect").addClass("cross");
@@ -20,29 +24,43 @@ function answerQuestion() {
     var userAnswer = document.getElementById("answer").value;
     if (answer[0] == userAnswer) {
         $("#right").removeClass("tick").addClass("correct");
+        correctAnswer = true;
     }
     else {
          $("#wrong").removeClass("cross").addClass("incorrect");
          $("#showAnswer").text(answer[0]);
+         wrongAnswer = true;
     }
     
 
 }
 
 function timer() {
-    var countdown = setInterval(timeDown, 1000);
-    document.getElementById("timer-numbers").innerHTML = 10;
-    function timeDown() {
-        var time = document.getElementById("timer-numbers").innerHTML;
-        time = time - 1
-        document.getElementById("timer-numbers").innerHTML = time;
-        if (time == 0) {
-            clearTimeout(countdown)
-            $("#slow").text("Too slow!")
+        wrongAnswer = false;
+        $("#slow").text("")
+        var countdown = setInterval(timeDown, 1000);
+        document.getElementById("timer-numbers").innerHTML = 10;
+        function timeDown() {
+            if (correctAnswer == false && wrongAnswer == false) {
+            var time = document.getElementById("timer-numbers").innerHTML;
+            time = time - 1
+            document.getElementById("timer-numbers").innerHTML = time;
+            if (time == 0) {
+                clearTimeout(countdown);
+                $("#slow").text("Too slow!");
+            }
+            if (correctAnswer == true) {
+                clearTimeout(countdown);
+                return time;
         }
+            if (wrongAnswer == true) {
+                clearTimeout(countdown);
+                return time;
+            }
+    }
+        
     }
 }
-
 
 
 $(document).ready(function() {
