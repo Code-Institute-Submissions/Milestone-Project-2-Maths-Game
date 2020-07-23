5cd1e50fbb811ce934d535ddb4d4779ed503e987
mphil17 <mattphil48@gmail.com>
2020-07-23 11:32:50 +0000

    Resized some elements so that all the important information could be seen at once. For the same reason, moved some elements around.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,19 @@
 let number1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 let number2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
+/*an array for keeping the score*/
+let score = [0];
+
 /*somewhere to store the correct answer so that it can be compared to the user's answer*/
 let answer = [];
 
 /*for the timer to know when to stop*/
 var countdown;
 
+var time;
+
 function setQuestion() {
 /*resets everything*/    
-    correctAnswer = false;
-    wrongAnswer = false;
     answer.length = 0;
     $("#right").removeClass("correct").addClass("tick");
     $("#wrong").removeClass("incorrect").addClass("cross");
@@ -34,44 +37,49 @@ function answerQuestion() {
 /*compares user's answer to the real answer and gives a tick or cross*/
     if (answer[0] == userAnswer) {
         $("#right").removeClass("tick").addClass("correct");
-/*so that the timer knows that the correct answer has been put in and stops*/
-        correctAnswer = true;
     }
     else {
          $("#wrong").removeClass("cross").addClass("incorrect");
 /*shows the real answer to the user so that they can learn*/
          $("#showAnswer").text(answer[0]);
-/*lets the timer know that the wrong answer has been put in and so it can stop*/
-         wrongAnswer = true;
     }
     
 
 }
 
 function timer() {
-/*resets the text*/
+/*resets the text and timer colour*/
         $("#slow").text("");
+        $("#timer-numbers").removeClass("time-running-out").addClass("text-style");
 /*runs timeDown function on a loop every second*/
         countdown = setInterval(timeDown, 1000);
 /*sets timer to 10 seconds*/
         document.getElementById("timer-numbers").innerHTML = 10;
  }   
         function timeDown() {
-/*the timer runs only if no answer has been given*/
-            if (correctAnswer == false && wrongAnswer == false) {
-/*this is for the user to see the timer and it goes down one every time the function is run*/ 
                 var time = document.getElementById("timer-numbers").innerHTML;
                 time = time - 1
                 document.getElementById("timer-numbers").innerHTML = time;
+/*timer goes red with 3 seconds left*/
+            if (time <= 3) {
+                $("#timer-numbers").removeClass("text-style").addClass("time-running-out");
+            }      
 /*when the timer reaches 0 it stops and gives a message*/
             if (time == 0) {
                 clearInterval(countdown);
                 $("#slow").text("Too slow!");
-            }  
+            }
+    
         
     }
-}
+
 
 function stopTimer() {
     clearInterval(countdown);
+    document.getElementById("playerScore").innerHTML = `Score: ${totalScore}`;
 }
+
+var totalScore = score.reduce(function (a, b) {
+    return a + b;
+});
+
